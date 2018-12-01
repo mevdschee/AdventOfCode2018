a4e18668db28839ccf96beb39f164fdd4e9b9b11
Maurits van der Schee <maurits@vdschee.nl>
2018-12-01 18:21:51 +0100

    day01

--- day01/part2.rb
+++ day01/part2.rb
@@ -1,13 +1,9 @@
 lines = File.readlines('input')
 sum = 0
 seen = {}
-done = false
-until done
-  lines.each do |_line|
-    sum += _line.to_i
-    done = true if seen[sum]
-    seen[sum] = true
-    break if done
-  end
+lines.cycle do |_line|
+  sum += _line.to_i
+  break if seen[sum]
+  seen[sum] = true
 end
 puts sum
