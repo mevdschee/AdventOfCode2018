54afc8525005ac0368e22a3f062391ba29edae7f
Maurits van der Schee <maurits@vdschee.nl>
2018-12-02 12:13:50 +0100

    day02

--- day02/part2.rb
+++ day02/part2.rb
@@ -1,7 +1,10 @@
 lines = File.readlines('input')
 common = ''
 lines.product(lines).map do |s1, s2|
-  common = s1.split('').zip(s2.split('')).map { |c1, c2| c1 if c1 == c2 }.join('')
+  common = ''
+  s1.length.times do |i|
+    common += s1[i] if s1[i] == s2[i]
+  end
   break if common.length == s1.length - 1
 end
 puts common
