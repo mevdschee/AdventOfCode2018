e61fe293b08b400b766bca1dcbbd219b7e165592
Maurits van der Schee <maurits@vdschee.nl>
2018-12-04 21:33:56 +0100

    day04

--- day04/part1.rb
+++ day04/part1.rb
@@ -3,7 +3,7 @@ guards = Hash.new { |h, k| h[k] = Hash.new(0) }
 start = 0; id = 0
 lines.sort.each do |_line|
   min = _line.match(/:(\d+)\]/).to_a[1].to_i
-  id = _line.match(/#(\d+)/).to_a[1].to_i if _line.include? 'begins shift'
+  id = _line.match(/#(\d+) /).to_a[1].to_i if _line.include? 'begins shift'
   start = min if _line.include? 'falls asleep'
   (start...min).each { |i| guards[id][i] += 1 } if _line.include? 'wakes up'
 end
