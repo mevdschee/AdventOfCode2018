632bcafbca8a4f29ac985b99f2f31c4a5da22497
Maurits van der Schee <maurits@vdschee.nl>
2018-12-04 22:08:36 +0100

    day04

--- day04/part2.rb
+++ day04/part2.rb
@@ -1,6 +1,6 @@
 lines = File.readlines('input')
 guards = Hash.new { |guards, guard| guards[guard] = Hash.new(0) }
-start = 0; guard = 0
+start = guard = 0
 lines.sort.each do |_line|
   minute = _line.match(/:(\d+)\]/).to_a[1].to_i
   guard = _line.match(/#(\d+) /).to_a[1].to_i if _line.include? 'begins shift'
