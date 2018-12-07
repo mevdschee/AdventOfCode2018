668b31b87872130d27253e26e5524d4220afff2a
Maurits van der Schee <maurits@vdschee.nl>
2018-12-07 10:49:06 +0100

    day07

--- day07/part1.rb
+++ day07/part1.rb
@@ -7,24 +7,19 @@ lines.each do |_line|
   reverse[_words[7]] << _words[1]
 end
 
-forward.map { |k, v| [k, v.sort] }
-
-def find_root(reverse)
-  _root = reverse.keys.first
+_next = []
+forward.keys.each do |_root|
   _root = reverse[_root].first while reverse[_root].count > 0
-  _root
-end
-
-def find_order(_order, _root, forward, reverse)
-  required = reverse[_root] - _order
-  if required.count==0
-    _order << _root
-    forward[_root].each do |_curr|
-      find_order(_order, _curr, forward, reverse)
-    end
-  end
+  _next << _root unless _next.include?(_root)
 end
 
 _order = []
-find_order(_order, find_root(reverse), forward, reverse)
+while _next.count>0
+  _ready = _next.select { |_root| (reverse[_root] - _order).count==0 }
+  _expand = _ready.sort.first
+  _order << _expand
+  _next.delete(_expand)
+  _next += forward[_expand]
+end
+
 puts _order.join('')
