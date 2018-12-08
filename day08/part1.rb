6ee49c65113ff31d477e676ce1a77533d2f105bb
Maurits van der Schee <maurits@vdschee.nl>
2018-12-08 07:15:47 +0100

    day08

---
+++ day08/part1.rb
@@ -0,0 +1,20 @@
+input = IO.read('input').chomp
+numbers = input.split(' ').map(&:to_i)
+
+def parse(_numbers)
+  node = { meta: [], children: [] }
+  child_count = _numbers.shift
+  meta_length = _numbers.shift
+  child_count.times do
+    node[:children] << parse(_numbers)
+  end
+  node[:meta] = _numbers.shift(meta_length)
+  node
+end
+
+def collect_meta(_node)
+  _node[:meta] + _node[:children].map { |_child| collect_meta(_child) }.flatten
+end
+
+root = parse(numbers)
+p collect_meta(root).sum
---
+++ day08/part2.rb
@@ -0,0 +1,27 @@
+input = IO.read('input').chomp
+numbers = input.split(' ').map(&:to_i)
+
+def parse(_numbers)
+  node = { meta: [], children: [], value: 0 }
+  child_count = _numbers.shift
+  meta_length = _numbers.shift
+  child_count.times do
+    node[:children] << parse(_numbers)
+  end
+  node[:meta] = _numbers.shift(meta_length)
+  node[:value] = if child_count == 0
+                   node[:meta].sum
+                 else
+                   node[:meta].reduce(0) do |_t, _i|
+                     _t + if node[:children][_i - 1]
+                            node[:children][_i - 1][:value]
+                          else
+                            0
+                          end
+                   end
+                 end
+  node
+end
+
+root = parse(numbers)
+p root[:value]
