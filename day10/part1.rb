f1e4aa1e464c5e6de0d53e70d0d807274259a6cd
Maurits van der Schee <maurits@vdschee.nl>
2018-12-10 22:54:50 +0100

    day10

--- day10/part1.rb
+++ day10/part1.rb
@@ -1,4 +1,4 @@
-lines = File.readlines('input')
+lines = File.readlines('input.2')
 points = lines.map { |_line| _line.scan(/(-?[0-9]+)/).to_a.flatten.map(&:to_i) }
 
 # loop while bbox shrinks
@@ -19,10 +19,50 @@ points.map! { |pn| [pn[0] - pn[2], pn[1] - pn[3]] }
 coords = Hash[points.collect { |point| [point, true] }]
 bbox = (0..3).to_a.map { |i| points.max_by { |c| (i < 2 ? -1 : 1) * c[i % 2] }[i % 2] }
 
-# print letters
+# find letters
+field = []
 (bbox[1]..bbox[3]).each do |y|
+  str = ' '
   (bbox[0]..bbox[2]).each do |x|
-    print (coords[[x, y]] ? '#' : ' ')
+    str += (coords[[x, y]] ? '#' : ' ')
   end
-  puts
+  field << str + ' '
 end
+
+# puts field
+
+alphabet = [
+  '   ##    #####    ####   #####   ######  ######   ####   #    #  ######     ###  #    #  #       #    #  #    #   ####   #####    ####   #####    #####  ######  #    #  #    #  #    #  #    #  #    #  ###### ',
+  '  #  #   #    #  #    #  #    #  #       #       #    #  #    #    ##        #   #   #   #       ##  ##  ##   #  #    #  #    #  #    #  #    #  #         ##    #    #  #    #  #    #  #    #  #    #       # ',
+  ' #    #  #    #  #       #    #  #       #       #       #    #    ##        #   #  #    #       ##  ##  ##   #  #    #  #    #  #    #  #    #  #         ##    #    #  #    #  #    #   #  #   #    #       # ',
+  ' #    #  #    #  #       #    #  #       #       #       #    #    ##        #   # #     #       # ## #  # #  #  #    #  #    #  #    #  #    #  #         ##    #    #  #    #  #    #   #  #    #  #       #  ',
+  ' #    #  #####   #       #    #  #####   #####   #       ######    ##        #   ##      #       # ## #  # #  #  #    #  #####   #    #  #####    ####     ##    #    #  #    #  #    #    ##     #  #      #   ',
+  ' ######  #    #  #       #    #  #       #       #  ###  #    #    ##        #   ##      #       #    #  #  # #  #    #  #       #    #  #  #         #    ##    #    #   #  #   # ## #    ##      ##      #    ',
+  ' #    #  #    #  #       #    #  #       #       #    #  #    #    ##        #   # #     #       #    #  #  # #  #    #  #       #    #  #   #        #    ##    #    #   #  #   # ## #   #  #     ##     #     ',
+  ' #    #  #    #  #       #    #  #       #       #    #  #    #    ##    #   #   #  #    #       #    #  #   ##  #    #  #       #  # #  #   #        #    ##    #    #   #  #   ##  ##   #  #     ##    #      ',
+  ' #    #  #    #  #    #  #    #  #       #       #   ##  #    #    ##    #   #   #   #   #       #    #  #   ##  #    #  #       #   ##  #    #       #    ##    #    #    ##    ##  ##  #    #    ##    #      ',
+  ' #    #  #####    ####   #####   ######  #        ### #  #    #  ######   ###    #    #  ######  #    #  #    #   ####   #        #####  #    #  #####     ##     ####     ##    #    #  #    #    ##    ###### '
+]
+
+letters = {}
+(0...26).each do |i|
+  letter = ''
+  (0...10).each do |j|
+    letter += "\n" + alphabet[j][i * 8, 8]
+  end
+  letters[letter] = ('A'.ord + i).chr
+end
+
+(0...(field[0].length / 8)).each do |i|
+  letter = ''
+  (0...10).each do |j|
+    letter += "\n" + field[j][i * 8, 8]
+  end
+  if letters[letter]
+    print letters[letter]
+  else
+    puts letter
+  end
+end
+
+puts
