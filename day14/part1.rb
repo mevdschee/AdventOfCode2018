c7e490db8a24b148b0ce4fb949bdf05d8279abdf
Maurits van der Schee <maurits@vdschee.nl>
2018-12-14 10:28:49 +0100

    day14

--- day14/part1.rb
+++ day14/part1.rb
@@ -1,16 +1,21 @@
 input = IO.read('input').chomp.to_i
 
-recipes = { 0 => 3, 1 => 7 }
+recipes = '37'
 p1 = 0
 p2 = 1
 
 len = 2
-while len < input + 10
-  n1 = recipes[p1]
-  n2 = recipes[p2]
-  (n1 + n2).to_s.split('').map(&:to_i).each do |_n|
+done = false
+while !done
+  n1 = recipes[p1].to_i
+  n2 = recipes[p2].to_i
+  (n1 + n2).to_s.each_char do |_n|
     recipes[len] = _n
-    len += 1
+    len+=1
+    if len == input + 10
+      done = true
+      break
+    end
   end
   p1 = (p1 + 1 + n1) % len
   p2 = (p2 + 1 + n2) % len
@@ -26,4 +31,4 @@ while len < input + 10
   # puts
 end
 
-puts (input...input + 10).map { |_i| recipes[_i] }.join('')
+puts recipes[-10..-1]
