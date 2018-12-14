543adf1c1361c1706ebc385739c6805c7101966e
Maurits van der Schee <maurits@vdschee.nl>
2018-12-15 00:33:07 +0100

    day14

--- day14/part2.py
+++ day14/part2.py
@@ -1,9 +1,9 @@
-input = open('input').read().strip()
+input = bytearray(open('input').read().strip(),'UTF-8')
 inputLen = len(input)
 
 recipes = bytearray(b' '*1024*1024*1024)
-recipes[0] = b'3'
-recipes[1] = b'7'
+recipes[0] = ord('3')
+recipes[1] = ord('7')
 p1 = 0
 p2 = 1
 
@@ -14,7 +14,7 @@ while (not done):
   n2 = int(recipes[p2]-ord('0'))
   sum = str(n1 + n2)
   for n in sum:
-    recipes[pos] = n
+    recipes[pos] = ord(n)
     pos+=1
     if (recipes[pos-inputLen:pos] == input):
       done = True
@@ -22,4 +22,4 @@ while (not done):
   p1 = (p1 + 1 + n1) % pos
   p2 = (p2 + 1 + n2) % pos
 
-print pos - inputLen
+print(pos - inputLen)
