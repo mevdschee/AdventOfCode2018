136951bb49062d8e9a3e7b0c0bd465d7fb36901f
Maurits van der Schee <maurits@vdschee.nl>
2018-12-14 22:56:47 +0100

    day14

--- day14/part2.php
+++ day14/part2.php
@@ -8,22 +8,22 @@ $recipes[1] = '7';
 $p1 = 0;
 $p2 = 1;
 
-$len = 2;
+$pos = 2;
 $done = false;
 while (!$done) {
   $n1 = (int)$recipes[$p1];
   $n2 = (int)$recipes[$p2];
   $str = (string)($n1 + $n2);
   for($i=0; $i<strlen($str); $i++) {
-    $recipes[$len] = $str[$i];
-    $len++;
-    if (substr($recipes,$len-$inputLen, $inputLen) == $input) {
+    $recipes[$pos] = $str[$i];
+    $pos++;
+    if (substr($recipes,$pos-$inputLen, $inputLen) == $input) {
       $done = true;
       break;
     }
   }
-  $p1 = ($p1 + 1 + $n1) % $len;
-  $p2 = ($p2 + 1 + $n2) % $len;
+  $p1 = ($p1 + 1 + $n1) % $pos;
+  $p2 = ($p2 + 1 + $n2) % $pos;
 }
 
-echo ($len - $inputLen)."\n";
+echo ($pos - $inputLen)."\n";
