dbba68ec07ef78ee33106ca19b7ecb8bf121c4d9
Maurits van der Schee <maurits@vdschee.nl>
2018-12-15 09:07:59 +0100

    day14

--- day14/part2.rb
+++ day14/part2.rb
@@ -1,27 +1,23 @@
 input = IO.read('input').chomp
 input_len = input.length
 
-recipes = ' ' * 1024 * 1024 * 1024
-recipes[0] = '3'
-recipes[1] = '7'
+recipes = '37'
 p1 = 0
 p2 = 1
 
-pos = 2
 done = false
 until done
   n1 = recipes[p1].to_i
   n2 = recipes[p2].to_i
   (n1 + n2).to_s.each_char do |_n|
-    recipes[pos] = _n
-    pos += 1
-    if recipes[pos - input_len...pos] == input
+    recipes.concat(_n)
+    if recipes[-input_len..-1] == input
       done = true
       break
     end
   end
-  p1 = (p1 + 1 + n1) % pos
-  p2 = (p2 + 1 + n2) % pos
+  p1 = (p1 + 1 + n1) % recipes.length
+  p2 = (p2 + 1 + n2) % recipes.length
   # (0...recipes.length).each do |_p|
   #  if p1 == _p
   #    print '(' + recipes[_p] + ')'
@@ -34,4 +30,4 @@ until done
   # puts
 end
 
-puts pos - input_len
+puts recipes.length - input_len
