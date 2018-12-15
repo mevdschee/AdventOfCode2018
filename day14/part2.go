899f13ea3bb434d8b90544b8247270fd1410d598
Maurits van der Schee <maurits@vdschee.nl>
2018-12-15 20:16:16 +0100

    day14

--- day14/part2.go
+++ day14/part2.go
@@ -10,15 +10,11 @@ import (
 func main() {
 	file, _ := ioutil.ReadFile("input")
 	input := []byte(strings.TrimSpace(string(file)))
-	inputLen := len(input)
 
-	recipes := make([]byte, 1024*1024*1024)
-	recipes[0] = '3'
-	recipes[1] = '7'
+	recipes := []byte("37")
 	p1 := 0
 	p2 := 1
 
-	pos := 2
 	done := false
 	for !done {
 		n1 := recipes[p1] - '0'
@@ -31,15 +27,14 @@ func main() {
 			num = []byte{sum / 10, sum % 10}
 		}
 		for _, n := range num {
-			recipes[pos] = n + '0'
-			pos++
-			if pos > inputLen && bytes.Equal(recipes[pos-inputLen-1:pos-1], input) {
+			recipes = append(recipes, n + '0')
+			if len(recipes) > len(input) && bytes.Equal(recipes[len(recipes)-1-len(input):len(recipes)-1], input) {
 				done = true
 				break
 			}
 		}
-		p1 = (p1 + 1 + int(n1)) % pos
-		p2 = (p2 + 1 + int(n2)) % pos
+		p1 = (p1 + 1 + int(n1)) % len(recipes)
+		p2 = (p2 + 1 + int(n2)) % len(recipes)
 	}
-	fmt.Println(pos - inputLen - 1)
+	fmt.Println(len(recipes) - len(input) - 1)
 }
