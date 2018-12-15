ced9f379902b132b5a0b879a9096fb546bd11725
Maurits van der Schee <maurits@vdschee.nl>
2018-12-15 19:47:35 +0100

    day14

--- day14/part2.go
+++ day14/part2.go
@@ -21,25 +21,25 @@ func main() {
 	pos := 2
 	done := false
 	for !done {
-		n1 := int(recipes[p1] - '0')
-		n2 := int(recipes[p2] - '0')
+		n1 := recipes[p1] - '0'
+		n2 := recipes[p2] - '0'
 		sum := n1 + n2
 		var num []byte
 		if sum < 10 {
-			num = []byte{byte(sum) + '0'}
+			num = []byte{sum}
 		} else {
-			num = []byte{byte(sum/10) + '0', byte(sum%10) + '0'}
+			num = []byte{sum / 10, sum % 10}
 		}
 		for _, n := range num {
-			recipes[pos] = n
+			recipes[pos] = n + '0'
 			pos++
 			if pos > inputLen && bytes.Equal(recipes[pos-inputLen-1:pos-1], input) {
 				done = true
 				break
 			}
 		}
-		p1 = (p1 + 1 + n1) % pos
-		p2 = (p2 + 1 + n2) % pos
+		p1 = (p1 + 1 + int(n1)) % pos
+		p2 = (p2 + 1 + int(n2)) % pos
 	}
 	fmt.Println(pos - inputLen - 1)
 }
