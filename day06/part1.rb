a8e555615fab6caab631605a506c178cd3898fa9
Maurits van der Schee <maurits@vdschee.nl>
2018-12-16 22:15:46 +0100

    day06

--- day06/part1.rb
+++ day06/part1.rb
@@ -1,19 +1,19 @@
 lines = File.readlines('input')
-points = lines.map { |_line| _line.split(',').map(&:to_i) }
-max = points.max_by { |_coords| _coords.max }.max
+points = lines.map { |line| line.split(',').map(&:to_i) }
+max = points.max_by(&:max).max
 
 counts = Hash.new(0)
 infinites = Hash.new(0)
 values = (0..max).to_a
-values.product(values).each do |_c|
-  _distances = points.map.with_index do |_p,_i| 
-    [_i, (_p[0]-_c[0]).abs + (_p[1]-_c[1]).abs]
+values.product(values).each do |c|
+  distances = points.map.with_index do |p, i|
+    [i, (p[0] - c[0]).abs + (p[1] - c[1]).abs]
   end.to_h
-  _distance = _distances.values.min
-  _closest = _distances.select { |_,_v| _v==_distance }.keys
-  _i = _closest[0]
-  counts[_i] += 1 if (_closest.count==1)
-  infinites[_i] = -1 if _c[0]==0 || _c[1]==0 || _c[0]==max || _c[1]==max
+  distance = distances.values.min
+  closest = distances.select { |_, v| v == distance }.keys
+  i = closest[0]
+  counts[i] += 1 if closest.count == 1
+  infinites[i] = -1 if c[0] == 0 || c[1] == 0 || c[0] == max || c[1] == max
 end
 
 puts counts.merge(infinites).values.max
--- day06/part2.rb
+++ day06/part2.rb
@@ -1,14 +1,14 @@
 lines = File.readlines('input')
 points = lines.map { |_line| _line.split(',').map(&:to_i) }
-max = points.max_by { |_coords| _coords.max }.max
+max = points.max_by(&:max).max
 
 count = 0
 values = (0..max).to_a
-values.product(values).each do |_c|
-  _distance = points.reduce(0) do |_total,_p| 
-    _total + (_p[0]-_c[0]).abs + (_p[1]-_c[1]).abs
+values.product(values).each do |c|
+  _distance = points.reduce(0) do |total, p|
+    total + (p[0] - c[0]).abs + (p[1] - c[1]).abs
   end
-  count+=1 if _distance<10000
+  count += 1 if _distance < 10_000
 end
 
 puts count
