cefb7b5e50a31357c1c7ffefc53c7fd43c8221ec
Maurits van der Schee <maurits@vdschee.nl>
2018-12-16 22:22:32 +0100

    day06

--- day06/part2.rb
+++ day06/part2.rb
@@ -1,14 +1,14 @@
 lines = File.readlines('input')
-points = lines.map { |_line| _line.split(',').map(&:to_i) }
+points = lines.map { |line| line.split(',').map(&:to_i) }
 max = points.max_by(&:max).max
 
 count = 0
 values = (0..max).to_a
 values.product(values).each do |c|
-  _distance = points.reduce(0) do |total, p|
+  distance = points.reduce(0) do |total, p|
     total + (p[0] - c[0]).abs + (p[1] - c[1]).abs
   end
-  count += 1 if _distance < 10_000
+  count += 1 if distance < 10_000
 end
 
 puts count
