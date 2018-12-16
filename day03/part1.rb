d716e584479a4809043a00629ceb87a53099f6a5
Maurits van der Schee <maurits@vdschee.nl>
2018-12-16 22:13:38 +0100

    day03

--- day03/part1.rb
+++ day03/part1.rb
@@ -1,8 +1,8 @@
 lines = File.readlines('input')
 re = /#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/
 claim = Hash.new(0)
-lines.each do |_line|
-  id, dx, dy, sw, sh = _line.scan(re).to_a[0].map(&:to_i)
+lines.each do |line|
+  id, dx, dy, sw, sh = line.scan(re).to_a[0].map(&:to_i)
   (dx...(dx + sw)).each do |px|
     (dy...(dy + sh)).each do |py|
       pos = { x: px, y: py }
@@ -10,4 +10,4 @@ lines.each do |_line|
     end
   end
 end
-puts claim.count { |_, _count| _count > 1 }
+puts claim.count { |_, count| count > 1 }
--- day03/part2.rb
+++ day03/part2.rb
@@ -2,8 +2,8 @@ lines = File.readlines('input')
 re = /#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/
 claim = {}
 valid = {}
-lines.each do |_line|
-  id, dx, dy, sw, sh = _line.scan(re).to_a[0].map(&:to_i)
+lines.each do |line|
+  id, dx, dy, sw, sh = line.scan(re).to_a[0].map(&:to_i)
   valid[id] = true
   (dx...(dx + sw)).each do |px|
     (dy...(dy + sh)).each do |py|
