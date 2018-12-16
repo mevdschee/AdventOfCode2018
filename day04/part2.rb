be6bea6ae18ac3d1afeb41b686eed7bc62f8b593
Maurits van der Schee <maurits@vdschee.nl>
2018-12-16 22:14:10 +0100

    day04

--- day04/part2.rb
+++ day04/part2.rb
@@ -1,11 +1,11 @@
 lines = File.readlines('input')
 guards = Hash.new { |guards, guard| guards[guard] = Hash.new(0) }
 start = guard = 0
-lines.sort.each do |_line|
-  minute = _line.match(/:(\d+)\]/).to_a[1].to_i
-  guard = _line.match(/#(\d+) /).to_a[1].to_i if _line.include? 'begins shift'
-  start = minute if _line.include? 'falls asleep'
-  (start...minute).each { |i| guards[guard][i] += 1 } if _line.include? 'wakes up'
+lines.sort.each do |line|
+  minute = line.match(/:(\d+)\]/).to_a[1].to_i
+  guard = line.match(/#(\d+) /).to_a[1].to_i if line.include? 'begins shift'
+  start = minute if line.include? 'falls asleep'
+  (start...minute).each { |i| guards[guard][i] += 1 } if line.include? 'wakes up'
 end
 guard = guards.max_by { |_, v| v.values.max || 0 }[0]
 minute = guards[guard].max_by { |_, v| v }[0]
