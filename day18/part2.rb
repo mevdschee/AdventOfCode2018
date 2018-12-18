ffdb482c670ca306311df0affdca94f425b1766e
Maurits van der Schee <maurits@vdschee.nl>
2018-12-18 16:23:00 +0100

    day18

--- day18/part2.rb
+++ day18/part2.rb
@@ -1,11 +1,7 @@
 @field = {}
 
 def init(filename)
-  File.readlines(filename).each_with_index do |line, y|
-    line.chomp.split('').each_with_index do |char, x|
-      @field[[x,y]]=char
-    end
-  end
+  field_from_s(IO.read(filename).chomp)
 end
 
 def neighbours(ox,oy)
@@ -26,14 +22,40 @@ def count_neighbours(x,y,c)
   neighbours(x,y).select {|x,y| @field[[x,y]]==c}.count
 end
 
+def field_from_s(s)
+  s.split("\n").each_with_index do |line, y|
+    line.chomp.split('').each_with_index do |char, x|
+      @field[[x,y]]=char
+    end
+  end
+end
+
+def field_to_s
+  min_x = @field.keys.map(&:first).min
+  min_y = @field.keys.map(&:last).min
+  max_x = @field.keys.map(&:first).max
+  max_y = @field.keys.map(&:last).max
+  s = ''
+  (min_y..max_y).each do |y|
+    (min_x..max_x).each do |x|
+        s+= @field[[x, y]]
+    end
+    s+="\n"
+  end
+  s
+end
+
 def count_characters(characters)
   @field.keys.select do |x, y|
     characters.include?(@field[[x, y]])
   end.count
 end
 
+states = {}
 init('input')
-10.times do
+loop_start = 0
+loop_end = 0
+loop.with_index do |_, i|
   new_field = {}
   @field.each do |coordinates, char|
     x,y = coordinates
@@ -48,6 +70,16 @@ init('input')
     new_field[[x,y]] = char
   end
   @field = new_field
+  s = field_to_s
+  if states[s]!=nil
+    loop_start = states[s]
+    loop_end = i
+    break
+  end
+  states[s] = i
 end
 
+index = loop_start + (1000000000 - loop_start - 1) % (loop_end - loop_start)
+
+field_from_s(states.invert[index])
 puts count_characters('|')*count_characters('#')
