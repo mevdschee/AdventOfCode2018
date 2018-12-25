bf45cf8b0e895f3258a313a5b2c43397333c804b
Maurits van der Schee <maurits@vdschee.nl>
2018-12-25 08:47:31 +0100

    day25

--- day25/part1.rb
+++ day25/part1.rb
@@ -1,5 +1,3 @@
-require 'set'
-
 def read_points(filename)
   lines = File.readlines(filename)
   re = /(-?\d+),(-?\d+),(-?\d+),(-?\d+)/
@@ -13,24 +11,29 @@ end
 
 def find_collisions(points, distance)
   indices = points.length.times.to_a
-  collisions = Hash.new { |h, k| h[k] = Set.new }
+  collisions = Hash.new { |h, k| h[k] = Hash.new(false) }
   indices.product(indices) do |point1, point2|
+    next if point1 < point2
     x1, y1, z1, r1 = points[point1]
     x2, y2, z2, r2 = points[point2]
-    collisions[point1] << point2 if (x2 - x1).abs + (y2 - y1).abs + (z2 - z1).abs + (r2 - r1).abs <= distance
+    c = if (x2 - x1).abs + (y2 - y1).abs + (z2 - z1).abs + (r2 - r1).abs <= distance
+    collisions[point1][point2] = c
+    collisions[point2][point1] = c
   end
   collisions
 end
 
 def find_constellations(collisions)
   collisions.each do |_, points1|
-    points1.to_a.each do |point2|
-      points2 = collisions[point2]
-      points1.merge(points2)
-      points2.merge(points1)
+    points1.keys.each do |point1|
+      points2 = collisions[point1]
+      points2.keys.each do |point2|
+        collisions[point1][point2] ||= collisions[point2][point1]
+        collisions[point2][point1] ||= collisions[point1][point2]
+      end
     end
   end
-  collisions.values.uniq
+  p collisions
 end
 
 points = read_points('input')
